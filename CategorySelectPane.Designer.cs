86f58ddea72e6a9b8a2f17d066a92b5b719e3138
bafedm <bafedm@gmail.com>
2023-02-09 11:50:16 -0700

    restored cbl font size due to v.scroll problems

--- CategorySelectPane.Designer.cs
+++ CategorySelectPane.Designer.cs
@@ -51,13 +51,12 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.cblCategoryList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.cblCategoryList.CheckOnClick = true;
-            this.cblCategoryList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cblCategoryList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cblCategoryList.FormattingEnabled = true;
             this.cblCategoryList.HorizontalScrollbar = true;
             this.cblCategoryList.Location = new System.Drawing.Point(0, 80);
             this.cblCategoryList.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
             this.cblCategoryList.Name = "cblCategoryList";
-            this.cblCategoryList.ScrollAlwaysVisible = true;
             this.cblCategoryList.Size = new System.Drawing.Size(175, 376);
             this.cblCategoryList.Sorted = true;
             this.cblCategoryList.TabIndex = 1;
